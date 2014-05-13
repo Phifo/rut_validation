4dbd2ff8cdbb4cec0e0b46b307635f4a53ffea90
muzk <nggomez@uc.cl>
2014-05-13 12:49:44 -0400

    patched RutValidator for nil values and non-string values

--- lib/validator.rb
+++ lib/validator.rb
@@ -6,8 +6,10 @@ class RutValidator < ActiveModel::EachValidator
   ##
   # @param value [String] the string to be validated
   def validate_each(record, attribute, value)
-    unless value.rut_valid?
-      record.errors[attribute] << (options[:message] || "is not a valid rut")
+    unless value.nil?
+      unless value.rut_valid?
+        record.errors[attribute] << (options[:message] || "is not a valid rut")
+      end
     end
   end
 end
